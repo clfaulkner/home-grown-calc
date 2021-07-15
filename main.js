aae8e14aa1fbc3a88850571975968ad34c0099f0
Chris Faulkner <clf.faulkner@gmail.com>
2021-07-15 15:13:21 -0700

    begin version 1

---
+++ main.js
@@ -0,0 +1,12 @@
+function calcIt(){
+  let num1=document.getElementById("box1").value,
+    num2=document.getElementById("box2").value,
+    operator=document.getElementById("operator").value;
+    console.log('num1: ', num1);
+    console.log('num2: ', num2);
+    console.log('operator: ', operator);
+// NOTE switch/case experiment
+const expr=operator;
+switch
+  
+}
